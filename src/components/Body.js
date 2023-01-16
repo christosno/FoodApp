4ca3f859a60fbe87bc988ea9afeb593de65a90ba
christosno <christosno1984@gmail.com>
2023-01-16 09:52:27 +0200

    api call for restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
-import { restaurantList } from "../constants";
+// import { restaurantList } from "../constants";
+
+// "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
 
 const filterRestaurants = (restaurantList, inputValue) => {
   if (inputValue === "") {
@@ -14,16 +16,34 @@ const filterRestaurants = (restaurantList, inputValue) => {
 
 const Body = () => {
   const [inputValue, setInputValue] = useState("");
-  const [restaurants, setRestaurants] = useState(restaurantList);
+  const [restaurants, setRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [isSearchClicked, setIsSearchClikted] = useState(false);
+
+  useEffect(() => {
+    callForRestaurants();
+  }, []);
+
+  const callForRestaurants = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const restaurantData = await data.json();
+    console.log(restaurantData.data.cards[2].data.data.cards);
+    const restaurantList = restaurantData.data.cards[2].data.data.cards;
+    setRestaurants(restaurantList);
+    setFilteredRestaurants(restaurantList);
+  };
 
   const inputHandler = (e) => {
     setInputValue(e.target.value);
     console.log(inputValue);
   };
 
-  const clickHandler = () => {
-    const fiteredRestaurants = filterRestaurants(restaurantList, inputValue);
-    setRestaurants(fiteredRestaurants);
+  const searchHandler = () => {
+    setIsSearchClikted(true);
+    const fiteredRestaurants = filterRestaurants(restaurants, inputValue);
+    setFilteredRestaurants(fiteredRestaurants);
   };
 
   return (
@@ -35,14 +55,27 @@ const Body = () => {
           value={inputValue}
           onChange={inputHandler}
         />
-        <button onClick={clickHandler}>Search</button>
+        <button onClick={searchHandler}>Search</button>
       </div>
       <div className="restaurant-list">
-        {restaurants.map((restaurant) => {
-          return (
-            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
-          );
-        })}
+        {!isSearchClicked && filteredRestaurants.length === 0 ? (
+          <div>Loading......</div>
+        ) : (
+          <>
+            {filteredRestaurants.length === 0 ? (
+              <div>No restaurants was found</div>
+            ) : (
+              filteredRestaurants.map((restaurant) => {
+                return (
+                  <RestaurantCard
+                    {...restaurant.data}
+                    key={restaurant.data.id}
+                  />
+                );
+              })
+            )}
+          </>
+        )}
       </div>
     </>
   );
