9103516dd26f18977d8f3306c85d22dcfa39aba2
christosno <christosno1984@gmail.com>
2023-02-16 22:58:14 +0200

    style the cart button in order to move at the end of viewport for small screen

--- src/components/HeaderCardButton.js
+++ src/components/HeaderCardButton.js
@@ -24,14 +24,14 @@ const HeaderCardButton = ({ onOpenModal, numItems }) => {
 
   return (
     <button
-      id={buttonId}
+      // id={buttonId}
       onClick={onOpenModal}
-      className="cursor-pointer font-bold border border-transparent bg-slate-900 text-white py-2 px-6 rounded hover:bg-slate-800 active:bg-slate-800 duration-500"
+      className="fixed left-1/2 transform -translate-x-1/2 bottom-6 md:static md:translate-x-0 cursor-pointer font-bold border border-white bg-slate-900 text-white py-2 px-6 rounded hover:bg-slate-800 active:bg-slate-800 duration-500"
     >
       <span className="w-5 h-5 mr-2">
         <FontAwesomeIcon icon={faShoppingCart} />
       </span>
-      <span className="text-white text-lg font-[Poppins]">Your Cart</span>
+      <span className="text-white text-lg font-[Poppins]">Cart</span>
       <span className="badge ml-2 bg-slate-700 text-white font-[Poppins] rounded py-1 px-2 font-bold">
         {numItems}
       </span>
